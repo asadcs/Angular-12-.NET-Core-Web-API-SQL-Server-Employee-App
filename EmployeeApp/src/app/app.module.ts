40f8d53df4c2a65aa351df8bec32eda30050c447
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-06-17 13:08:01 -0700

    Department and Employee Model Created

--- EmployeeApp/src/app/app.module.ts
+++ EmployeeApp/src/app/app.module.ts
@@ -4,10 +4,26 @@ import { MatSliderModule } from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { EditEmpComponent } from './employee/edit-emp/edit-emp.component';
+import { AddEmpComponent } from './employee/add-emp/add-emp.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDeptComponent } from './department/show-dept/show-dept.component';
+import { EditDeptComponent } from './department/edit-dept/edit-dept.component';
+import { AddDeptComponent } from './department/add-dept/add-dept.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    EditEmpComponent,
+    AddEmpComponent,
+    DepartmentComponent,
+    ShowDeptComponent,
+    EditDeptComponent,
+    AddDeptComponent
   ],
   imports: [
     BrowserModule,
